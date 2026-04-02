dbbc8473293c33ab463dd579dc15e90782b88674
fixl-developer <develop@fixlsolutions.com>
2026-04-02 22:06:57 +0530

    Allow all origins for CORS

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: [process.env.FRONTEND_URL, process.env.ADMIN_URL].filter(Boolean),
+  origin: true,
   credentials: true,
 }));
 app.use(express.json({ limit: "10mb" }));
